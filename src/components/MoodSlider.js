8859d024d79a3c577e7ffdc4bd076671363d2388
Isaiah Mann <isaiahbmann@gmail.com>
2020-07-19 17:07:29 -0700

    Set mood variable in slider

--- src/components/MoodSlider.js
+++ src/components/MoodSlider.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import './MoodSlider.css'
 
+const labels = ['Very Low', 'Low', 'Somewhat Low', 'Neutral', 'Somewhat High', 'High', 'Very High'];
+
 class MoodSlider extends React.Component {
   constructor(props) {
     super(props);
+    const neutralIdx = Math.floor(labels.length / 2);
     this.state = {
-      value: 0
+      value: neutralIdx,
+      mood: labels[neutralIdx]
     };
   }
   render() {
     const { value } = this.state;
-    const labels = ['Extreme Low', 'Mid Low', 'Low', 'Neutral', 'High', 'Mid High', 'Extreme High'];
     return (
       <div className='mood-slider'>
         <div className='mood-title'>Mood</div>
         <div className='mood-slider-container'>
           <input type='range' min='0' max={labels.length - 1} value={value} className='mood-slider-bar' id='myRange'  onChange={v => {
-              console.log(v.target.value)
-              this.setState({ value: v.target.value });
-              }}/>
+              const idx = v.target.value;
+              this.setState({ value: idx, mood: labels[idx] });
+            }}/>
         </div>
         <div className='mood-label'>{labels[value]}</div>
       </div>
