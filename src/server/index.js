6b2845bbf10da6a2c9752235a9ed4b8985f21fd1
Isaiah Mann <isaiahbmann@gmail.com>
2022-04-14 22:56:03 -0700

    [WIP] Add Postgres session table

--- src/server/index.js
+++ src/server/index.js
@@ -2,9 +2,30 @@ require('dotenv').config();
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oidc');
+const sessionPool = require('pg').Pool
+const session = require('express-session')
+const pgSession = require('connect-pg-simple')(session)
+const ensureLogIn = require('connect-ensure-login').ensureLoggedIn;
 const app = express();
 const port = process.env.PORT || 3000;
-const router = express.Router();
+
+const sessionDBaccess = new sessionPool({
+  user: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 5432,
+  database: process.env.DB_NAME || 'postgres'
+})
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  store: new pgSession({
+    pool: sessionDBaccess,
+    tableName: 'session'
+  })
+}));
 
 passport.use(new GoogleStrategy({
   clientID: process.env['GOOGLE_CLIENT_ID'],
@@ -15,12 +36,36 @@ passport.use(new GoogleStrategy({
   return cb(null, {});
 }));
 
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    cb(null, { id: user.id, username: user.username, name: user.name });
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-router.get('/login', function(req, res, next) {
+app.get('/login', function(req, res, next) {
   res.render('login');
+});
+
+app.get('/login/federated/google', passport.authenticate('google'));
+
+app.get('/oauth2/redirect/google', passport.authenticate('google', {
+  successReturnToOrRedirect: '/',
+  failureRedirect: '/'
+}));
+
+app.post('/logout', function(req, res, next) {
+  req.logout();
+  res.redirect('/');
 });
 
 app.listen(port, () => {
