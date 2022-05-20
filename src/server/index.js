5db3453d5877fe5176390f1790a8493bc1d6bd98
Isaiah Mann <isaiahbmann@gmail.com>
2022-05-20 09:52:56 -0400

    [WIP] Add in db init calls

--- src/server/index.js
+++ src/server/index.js
@@ -3,21 +3,40 @@ const path = require('path');
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oidc');
-const sessionPool = require('pg').Pool
+const pg = require('pg');
+const sessionPool = pg.Pool;
 const session = require('express-session')
 const pgSession = require('connect-pg-simple')(session)
 const ensureLogIn = require('connect-ensure-login').ensureLoggedIn;
 const app = express();
 const port = process.env.PORT || 3001;
 const DEBUG_MODE = process.env.DEBUG_MODE || true;
-
-const sessionDBaccess = new sessionPool({
+const dbConfig = {
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 5432,
   database: process.env.DB_NAME || 'postgres'
-})
+};
+
+const sessionDBaccess = new sessionPool(dbConfig);
+const db = new pg.Client(dbConfig);
+
+db.query("CREATE TABLE IF NOT EXISTS users ( \
+  id INTEGER PRIMARY KEY, \
+  username TEXT UNIQUE, \
+  hashed_password BLOB, \
+  salt BLOB, \
+  name TEXT \
+)");
+
+db.query("CREATE TABLE IF NOT EXISTS federated_credentials ( \
+  id INTEGER PRIMARY KEY, \
+  user_id INTEGER NOT NULL, \
+  provider TEXT NOT NULL, \
+  subject TEXT NOT NULL, \
+  UNIQUE (provider, subject) \
+)");
 
 app.use(session({
   secret: 'keyboard cat',
@@ -34,8 +53,40 @@ passport.use(new GoogleStrategy({
   clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
   callbackURL: '/oauth2/redirect/google',
   scope: [ 'profile' ]
-}, function(issuer, profile, cb) {
-  return cb(null, {});
+}, function verify(issuer, profile, cb) {
+  db.query('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
+    issuer,
+    profile.id
+  ], function(err, row) {
+    if (err) { return cb(err); }
+    if (!row) {
+      db.query('INSERT INTO users (name) VALUES (?)', [
+        profile.displayName
+      ], function(err) {
+        if (err) { return cb(err); }
+
+        var id = this.lastID;
+        db.query('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
+          id,
+          issuer,
+          profile.id
+        ], function(err) {
+          if (err) { return cb(err); }
+          var user = {
+            id: id,
+            name: profile.displayName
+          };
+          return cb(null, user);
+        });
+      });
+    } else {
+      db.query('SELECT * FROM users WHERE id = ?', [ row.user_id ], function(err, row) {
+        if (err) { return cb(err); }
+        if (!row) { return cb(null, false); }
+        return cb(null, row);
+      });
+    }
+  });
 }));
 
 passport.serializeUser(function(user, cb) {
