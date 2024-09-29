3568e66686c743b4f3c945a72e03f8c8c601ed23
Isaiah Mann <isaiahbmann@gmail.com>
2024-09-29 05:45:55 -0400

    Add rough Supabase auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import dotenv from 'dotenv'
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MoodForm from './components/MoodForm';
+import GoogleSignIn from './components/GoogleSignIn';
+
+dotenv.config()
 
 export default class App extends React.Component {
   render() {
     return (
       <Container>
+          <GoogleSignIn />
           <MoodForm/>
       </Container>
     );
---
+++ src/components/GoogleSignIn.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import { supabase } from '../lib/supabase'
+
+export default function GoogleSignIn() {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    async function checkUser() {
+      const { data: { session } } = await supabase.auth.getSession()
+      if (session.user) {
+        setUser(session.user)
+      }
+    }
+
+    checkUser()
+  }, [])
+
+  async function handleGoogleSignIn() {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+    })
+
+    if (error) {
+        console.error('Error signing in:', error.message)
+    }
+  }
+
+  if (user) {
+    return <p>Signed in as {user.email}</p>
+  }
+
+  return (
+    <button onClick={handleGoogleSignIn}>Sign in with Google</button>
+  )
+}
